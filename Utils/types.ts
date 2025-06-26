40a4ce9fcd8a94152728fbd444bb1702f3babcf1
Liamarjit Bhogal <liamarjit@sevacloud.co.uk>
2025-06-26 11:29:33 +0100

    Update types.ts
    Adding extra types for generic query hooks

--- Utils/types.ts
+++ Utils/types.ts
@@ -1,4 +1,16 @@
-export type ApiError<T> = {
-  data: T;
+export interface GraphQLResponse<TResponse> {
+  [key: string]: TResponse[] | null;
+}
+
+// This is the error object within React Query output
+export type ApiError<TResponse> = {
+  data: TResponse;
   errors: ErrorObject[];
 };
+
+interface ErrorObject {
+  path: number[] | string[];
+  errorType: string;
+  locations: any | null;
+  message: string;
+}
